81e2a130020723d711afbcf45f56ebec3a17a4ff
rinnegard <victor.rinnegard@hotmail.com>
2020-09-12 23:07:09 +0100

    Basic express test added

---
+++ app.js
@@ -0,0 +1,12 @@
+onst express = require("express");
+const app = express();
+
+const port = 1337;
+
+// Add a route
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+// Start up server
+app.listen(port, () => console.log(`Example API listening on port ${port}!`));
