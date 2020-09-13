fe98e47214478a01c73b911689a1fe4cfc86ca01
rinnegard <victor.rinnegard@hotmail.com>
2020-09-13 19:29:18 +0100

    Added route folder

--- app.js
+++ app.js
@@ -81,6 +81,22 @@ app.use((req, res, next) => {
     next(err);
 });
 
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.status || 500).json({
+        "errors": [
+            {
+                "status": err.status,
+                "title":  err.message,
+                "detail": err.message
+            }
+        ]
+    });
+});
+
 
 // Start up server
 app.listen(port, () => console.log(`Example API listening on port ${port}!`));
