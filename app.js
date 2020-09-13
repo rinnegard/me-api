a0fdec99c6fce8d8c0e8a739e095d2ad26288e2e
rinnegard <victor.rinnegard@hotmail.com>
2020-09-13 05:09:45 +0100

    Imported cors and morgan into app

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const morgan = require('morgan');
+const cors = require('cors');
 
+const app = express();
 const port = 1337;
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors());
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+// This is middleware called for all routes.
+// Middleware takes three parameters.
+app.use((req, res, next) => {
+    console.log(req.method);
+    console.log(req.path);
+    next();
+});
 
 // Add a route
 app.get("/", (req, res) => {
@@ -13,6 +34,44 @@ app.get("/", (req, res) => {
 
     res.json(data);
 });
+
+// Testing routes with method
+app.get("/user", (req, res) => {
+    res.json({
+        data: {
+            msg: "Got a GET request, sending back default 200"
+        }
+    });
+});
+
+app.post("/user", (req, res) => {
+    res.status(201).json({
+        data: {
+            msg: "Got a POST request, sending back 201 Created"
+        }
+    });
+});
+
+app.put("/user", (req, res) => {
+    // PUT requests should return 204 No Content
+    res.status(204).send();
+});
+
+app.delete("/user", (req, res) => {
+    // DELETE requests should return 204 No Content
+    res.status(204).send();
+});
+
+app.get("/hello/:msg", (req, res) => {
+    const data = {
+        data: {
+            msg: req.params.msg
+        }
+    };
+
+    res.json(data);
+});
+
 
 // Start up server
 app.listen(port, () => console.log(`Example API listening on port ${port}!`));
