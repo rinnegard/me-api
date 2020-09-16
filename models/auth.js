a79453832ca4e9c7d99d998f6dfaa62fe60bf0ad
rinnegard <victor.rinnegard@hotmail.com>
2020-09-16 04:49:51 +0100

    Started login auth

--- models/auth.js
+++ models/auth.js
@@ -51,6 +51,85 @@ const auth = {
         });
     },
 
+    login: function(res, email, password) {
+        if (!email || !password) {
+            return res.status(401).json({
+                errors: {
+                    status: 401,
+                    source: "/login",
+                    title: "Email or password missing",
+                    detail: "Email or password missing in request"
+                }
+            });
+        }
+
+        db.get("SELECT * FROM users WHERE email IS (?)",
+            email, (err, rows) => {
+                if (err) {
+                    return res.status(500).json({
+                        errors: {
+                            status: 500,
+                            source: "/login",
+                            title: "Database error",
+                            detail: err.message
+                        }
+                    });
+                }
+
+                if (rows === undefined) {
+                    return res.status(401).json({
+                        errors: {
+                            status: 401,
+                            source: "/login",
+                            title: "User not found",
+                            detail: "User with provided email not found."
+                        }
+                    });
+                }
+
+                bcrypt.compare(password, row.password, function(err, res) {
+                    if (err) {
+                        errors: {
+                            status: 500,
+                            source: "/login",
+                            title: "bcrypt error",
+                            detail: "bcrypt error"
+                        }
+                    }
+
+                    if (res) {
+                        return res.json({
+                            data: {
+                                type: "success",
+                                message: "User logged in",
+                                user: payload,
+                                token: jwtToken
+                            }
+                        });
+                    }
+
+
+                    return res.status(401).json({
+                        errors: {
+                            status: 401,
+                            source: "/login",
+                            title: "Wrong password",
+                            detail: "Password is incorrect."
+                        }
+                    });
+                })
+
+
+
+                return res.status(201).json({
+                    data: {
+                        message: "User successfully logged in."
+                    }
+                });
+            });
+    },
+
+
 }
 
 module.exports = auth;
