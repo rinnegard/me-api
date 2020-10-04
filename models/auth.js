94f982fbd190d7a51df7b5684005cb5721e88e35
Victor <victor.rinnegard@hotmail.com>
2020-10-04 22:53:58 +0100

    Added support for importing database based on if it's test or not

--- models/auth.js
+++ models/auth.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database("./db/texts.sqlite");
+const db = require("../db/database.js");
 const bcrypt = require('bcryptjs');
 require('dotenv').config()
 
