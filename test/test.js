fd623bcaa7ddded6759596115ebb95827e2ffa35
Victor <victor.rinnegard@hotmail.com>
2020-10-04 22:50:09 +0100

    Added basic test for getting a report

--- test/test.js
+++ test/test.js
@@ -1,8 +1,41 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app.js');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Reports', () => {
+    describe('GET /reports/week/1', () => {
+        it('201 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/reports/week/1")
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    res.body.data.should.be.an("object");
+                    res.body.data.result.week.should.be.equal(1);
+
+                    done();
+                });
+        });
     });
-  });
+
+    describe('GET /reports/week/2', () => {
+        it('201 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/reports/week/2")
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    res.body.data.should.be.an("object");
+                    res.body.data.result.week.should.be.equal(2);
+
+                    done();
+                });
+        });
+    });
 });
